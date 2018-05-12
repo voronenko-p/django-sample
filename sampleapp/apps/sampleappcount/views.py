fcf3a26ba7434ae29a5258e0ab29054cdc08bb9b
Vyacheslav Voronenko <git@voronenko.info>
2018-05-12 10:44:26 +0200

    chore(view) display serving python version

--- sampleapp/apps/sampleappcount/views.py
+++ sampleapp/apps/sampleappcount/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from sampleappcount.models import Visit
+import platform
 
 def say_hello(request):
     user_agent = request.META['HTTP_USER_AGENT']
+    python_version = platform.python_version()
     Visit.objects.create(user_agent=user_agent)
     return render(request, 'say_hello.html', {
         'count': Visit.objects.count(),
-        'user_agent': user_agent
+        'user_agent': user_agent,
+        'python_version': python_version
     })
